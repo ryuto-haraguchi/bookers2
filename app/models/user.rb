1e5e43ca4bb772344f43ae02533012ac37175641
EC2 Default User <ec2-user@ip-172-31-9-73.ap-southeast-2.compute.internal>
2024-09-24 10:26:24 +0000

    [update]login-uodate

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if name = conditions.delete(:name)
+        where(conditions.to_h).where(["name = :value", { value: name }]).first
+      end
+    end
 end
