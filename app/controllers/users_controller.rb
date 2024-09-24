9a229c7a475ea5ee7f2f000d12974c230f441c17
EC2 Default User <ec2-user@ip-172-31-9-73.ap-southeast-2.compute.internal>
2024-09-24 09:37:08 +0000

    [update]user-show-view,user-edit-view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    @user = User.find(current_user.id)
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :image)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'homes#home'
-  get 'home/about' => 'homes#about', as: 'about'
-  devise_for :users
 
+  root to: 'homes#home'
+
+  get 'home/about' => 'homes#about', as: 'about'
+
+  devise_for :users
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
