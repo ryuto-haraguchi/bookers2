316c244b811e80185245200129d31f4240729682
EC2 Default User <ec2-user@ip-172-31-9-73.ap-southeast-2.compute.internal>
2024-09-25 07:38:42 +0000

    [update]user-profile-image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 class User < ApplicationRecord
   has_many :books
-  has_one_attached :image
+  has_one_attached :profile_image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    def get_profile_image(width, height)
+      unless profile_image.attached?
+        file_path = Rails.root.join('app/assets/images/no_image.jpg')
+        profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      end
+      profile_image.variant(resize_to_limit: [width, height]).processed
+    end
 
     def self.find_for_database_authentication(warden_conditions)
       conditions = warden_conditions.dup
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'homes#home'
+
 
   get 'home/about' => 'homes#about', as: 'about'
 
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update, :index]
 
   resources :books
+
+  root to: 'homes#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
