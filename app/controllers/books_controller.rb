c723fee2ba684452ada4445b1eb8c5d97e78abd7
EC2 Default User <ec2-user@ip-172-31-9-73.ap-southeast-2.compute.internal>
2024-09-26 10:08:23 +0000

    [update]page統一化

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
-    @book.user_id = current_user.id
+    @user = @book.user.id
     @book.save
     redirect_to books_path
   end
@@ -9,10 +9,12 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
+    @user = current_user
   end
 
   def show
     @book_user = Book.find(params[:id])
+    @user = @book_user.user
     @book = Book.new
   end
 
